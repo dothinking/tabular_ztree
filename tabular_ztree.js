aabc9bb1f4c7d7c11591a6b5c129c084e0a9a81f
dothinking <skyway2011@yeah.net>
2018-02-25 20:20:44 +0800

    update configuration parameter for tabular zTree

--- tabular_ztree.js
+++ tabular_ztree.js
@@ -1,41 +1,76 @@
-function init_tabular_ztree(obj, setting, nodes, cols)
+/*
+** 利用addDiyDom初始化zTree
+*/
+function init_tabular_ztree(obj, zSetting, nodes, tSetting)
 {
-    obj.addClass("table_ztree");
+    // tabular_ztree配置参数
+    default_setting = {
+        cols:[{
+            key:"", 
+            name:"TREE_NODES", 
+            width:0.4
+        },{
+            key:"k1", 
+            name:"COL1", 
+            width:0.2
+        },{
+            key:"k2", 
+            name:"COL2", 
+            width:0.2
+        },{
+            key:"k3", 
+            name:"COL3", 
+            width:0.2
+        }],
 
-    // 自定义dom
-    setting.view.addDiyDom = function(treeId, treeNode){
-        diyDom(treeId, treeNode, cols)
+        config: {
+            switch: true, /*zTree展开/折叠图标*/
+            space: 20     /*不同层级基础边距*/
+        }
     };
 
-    // 初始化树
-    var treeObj = $.fn.zTree.init(obj, setting, nodes);
+    tSetting = $.extend(default_setting, tSetting);
+
+    // 自定义dom
+    zSetting.view.addDiyDom = function(treeId, treeNode){
+        diyDom(treeId, treeNode, tSetting)
+    };
+
+    // 初始化zTree
+    var treeObj = $.fn.zTree.init(obj, zSetting, nodes);
 
     // 添加表头
-    var header = treeHeader(cols);
+    var header = treeHeader(tSetting.cols);
     if (obj.find('li')) obj.prepend(header);
+
+    // 设置样式
+    obj.addClass("tabular_ztree");
 
     return treeObj;
 }
 
+/*
+** 根据配置参数生成表头结构
+*/
 function treeHeader(cols)
 {
-    var tree_head = '<li class="tree_head"><a>';
-    tree_head +='<div class="diy" style="width:'+cols.title.width*100+'%;">'+cols.title.name+'</div>';
-
-    for(k in cols.contents){
-        tree_head += '<div class="diy" style="width:'+cols.contents[k].width*100+'%;">'+cols.contents[k].name+'</div>';
+    var header = '<li class="tabular_head"><a>';
+    for(k in cols){
+        header += '<div class="tabular_col" style="width:'+cols[k].width*100+'%;">'+cols[k].name+'</div>';
     }
+    header += '</a></li>';
 
-    tree_head += '</a></li>';
-
-    return tree_head;
+    return header;
 }
 
-function diyDom(treeId, treeNode, cols)
+/*
+** 重新组织zTree节点结构，以便配合样式文件显示表格样式
+*/
+function diyDom(treeId, treeNode, tSetting)
 {    
-    var spaceWidth = 15; // 左边距控制
+    var spaceWidth = tSetting.config.space; // 左边距控制
 
-    // ztree节点主要dom        
+    // zTree节点主要dom        
     var liObj = $("#" + treeNode.tId);
     var switchObj = $("#" + treeNode.tId + "_switch"); // 展开/折叠图标
     var checkObj = $("#" + treeNode.tId + "_check");   // checkbox
@@ -50,23 +85,27 @@ function diyDom(treeId, treeNode, cols)
     spanObj.remove();              
 
     // 转移到<div>标签下作为第一列
-    var node = $('<div class="diy" style="width:'+cols.title.width*100+'%;"></div>');
+    var node = $('<div class="tabular_col" style="width:'+tSetting.cols[0].width*100+'%;"></div>');
     var spaceSpan = $('<span style="height:1px;display: inline-block;width:' + (spaceWidth * treeNode.level) + 'px"></span>');
     node.append(spaceSpan);
-    node.append(switchObj);
-    if(checkObj.length)node.append(checkObj);
+    if(tSetting.config.switch) node.append(switchObj);
+    if(checkObj.length) node.append(checkObj);
     node.append(icoObj);
     node.append(spanObj);
     aObj.append(node);
 
     // 添加新列
     var editStr = '';
-    for(k in cols.contents){
-        editStr += '<div class="diy" style="width:'+cols.contents[k].width*100+'%;">' + getData(treeNode[cols.contents[k].key], "") + '</div>';
+    for(k in tSetting.cols){
+        if(k==0) continue;
+        editStr += '<div class="tabular_col" style="width:'+tSetting.cols[k].width*100+'%;">' + getData(treeNode[tSetting.cols[k].key], "") + '</div>';
     }
     aObj.append(editStr);
 };
 
+/*
+** 获取节点属性值
+*/
 function getData(val, empty){
     return val==null ? empty : val;
 }
